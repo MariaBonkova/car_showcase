1cad01eeceb20357d5025176f44baebda3a2a5c2
MariaBonkova <mbonkova@mail.bg>
2024-02-14 22:20:43 +0200

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
     images: {
-       remotePatterns: ["cdn.imagin.studio"]
-    }
-}
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'cdn.imagin.studio',
+                port: '',
+                pathname: '/**',
 
-export default nextConfig
+            },
+        ],
+    },
+}
